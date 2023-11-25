63b6dce533a19f9fa930a04a20640f596e2e9e28
dqwins <darci.wright@uleth.ca>
2023-11-25 14:13:41 -0700

    change color of buttons

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,37 @@ console.log(x);
 
 $("#num-1").on("click", function () {
     $(this).append("<h2> Hairstyle 1 </h2>");
+    $(this).css("color", "white")
     $(this).addClass("hair1");
     $(this).unbind("click");
 });
 
 $("#num-2").on("click", function () {
     $(this).append("<h2> Hairstyle 2 </h2>");
+    $(this).css("color", "white")
     $(this).addClass("hair2");
     $(this).unbind("click");
 });
+
+$("#num-3").on("click", function () {
+    $(this).append("<h2> Hairstyle 3 </h2>");
+    $(this).css("color", "white")
+    $(this).addClass("hair3");
+    $(this).unbind("click");
+});
+
+$("#num-4").on("click", function () {
+    $(this).append("<h2> Hairstyle 4 </h2>");
+    $(this).css("color", "white")
+    $(this).addClass("hair4");
+    $(this).unbind("click");
+});
+
+$("#num-5").on("click", function () {
+    $(this).append("<h2> Hairstyle 5 </h2>");
+    $(this).css("color", "white")
+    $(this).addClass("hair5");
+    $(this).unbind("click");
+});
+
+
